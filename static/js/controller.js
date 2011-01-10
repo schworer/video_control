a898089f60d47d3d4534bc7eb8c5e64a727c54a0
Alex Schworer <alex.schworer@gmail.com>
2011-01-09 17:02:28 -0800

    add a comment so I remember what this does

--- static/js/controller.js
+++ static/js/controller.js
@@ -1,5 +1,7 @@
 $(window).keypress(function (event) {
     if (event.which == 32) {
+        // when spacebar is pressed, flip the state of play and send it
+        // to the server
         play ^= 1;
         if (play == true) {
             socket.send('play');
