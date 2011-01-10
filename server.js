cf6173197c03a9ba978663817c0da70e9f8b4cc0
Alex Schworer <alex.schworer@gmail.com>
2011-01-09 17:01:29 -0800

    keep track of play state on the server

--- server.js
+++ server.js
@@ -9,16 +9,26 @@ var server = Connect.createServer(
 );
 
 var socket = io.listen(server);
+var play = true;
 
 socket.on('connection', function(client) {
     // this pattern taken from creationix's hexes examples
     var cmds = {
         play: function () {
             socket.broadcast('play');
+            play = true;
         },
         pause: function () {
             socket.broadcast('pause');
+            play = false;
         }
+    }
+
+    // when a client connects, send the current play state
+    if (play == true) {
+        client.send('play');
+    } else {
+        client.send('pause');
     }
 
     client.on('message', function(command) {
